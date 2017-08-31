60d7a0a65e3b4ffb1ba0cce82c8a75c2b66175ff
Helena Strada <h.strada@hotmail.com>
2017-08-31 11:00:24 -0300

    Login ajustado

--- seguros-saas-web/src/main/java/br/com/fiap/segurossaas/controller/LoginController.java
+++ seguros-saas-web/src/main/java/br/com/fiap/segurossaas/controller/LoginController.java
@@ -3,6 +3,7 @@ package br.com.fiap.segurossaas.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -25,7 +26,7 @@ public class LoginController {
 		return "login";
 	}
 	
-	@RequestMapping("logar")
+	@PostMapping("logar")
 	public String logar(@RequestParam("usuario") String usuario, @RequestParam("senha") String senha, Model model ){
 		Loja loja = lojaService.buscaLoja(usuario, senha);
 		if(loja != null){
